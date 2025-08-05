7e0659aae072b4c8f581be7a794ef6d0a20e5d06
StevenAks21 <160478328+StevenAks21@users.noreply.github.com>
2025-08-05 14:01:05 +1000

    written code to make sure uploads folder exists

--- LifeLog/routes/upload.js
+++ LifeLog/routes/upload.js
@@ -2,8 +2,18 @@ const express = require(`express`)
 const requireAuth = require(`../middleware/requireAuth`)
 const path = require(`path`)
 const multer = require(`multer`)
-const {v4 : uuid} = require(`uuid`)
+const { v4: uuid } = require(`uuid`)
 const fs = require(`fs`)
+
+
+const uploadPath = path.join(__dirname, `..`, `uploads`)
+
+if(!fs.existsSync(uploadPath)){
+    fs.mkdirSync(uploadPath, {recursive: true})
+}
+
+
+
 
 
 const router = express.Router()
