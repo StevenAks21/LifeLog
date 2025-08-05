8688781f5b631460cb465df5d63764e6c079166c
StevenAks21 <160478328+StevenAks21@users.noreply.github.com>
2025-08-05 12:00:48 +1000

    Installed modules

--- LifeLog/app.js
+++ LifeLog/app.js
@@ -4,9 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -19,8 +16,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.get('/', (req, res) => {
+  res.status(200).json({hi:'hi'})
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +36,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+app.listen(3000)
 
 module.exports = app;
